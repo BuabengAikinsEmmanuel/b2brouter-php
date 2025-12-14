3816f0c3170ede4a3119f942c121ec3d904263b9
BuabengAikinsEmmanuel <darktechhub2@gmail.com>
2025-12-14 23:46:35 +0000

    Update README.md

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string fn;
+string ln;
+int age;
+int working;
+const int retire = 60;
+
+Console.WriteLine("Enter Your First Name: ");
+fn= Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Enter Your last Name: ");
+ln = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Enter Your age: ");
+age = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine();
+working = retire - age;
+Console.WriteLine();
+
+Console.WriteLine($"FULL NAME: {ln} {fn}");
+Console.WriteLine();
+Console.WriteLine($"AGE: {age}");
+Console.WriteLine();
+Console.WriteLine($"Working years remaing : {working}");
+
+
